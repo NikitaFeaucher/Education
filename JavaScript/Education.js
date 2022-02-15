308ea9ab29bf252c8a883ab5e5b6013634055048
NikitaFeaucher <nb49060@gmail.com>
2022-02-15 18:54:22 +0200

    4180

---
+++ JavaScript/Education.js
@@ -0,0 +1,41 @@
+//JavaScript - это язык программирования HTML-страниц и Web, являющийся самым популярным и легким.
+function Calculator()//Калькулятор
+{
+    $(".Calculator").show();
+}
+function CalculatorDrag()
+{
+    
+}
+function CalculatorDrop()
+{
+    
+}
+function Imparity()//Неравенства
+{
+    //
+}
+function Functions()//Математические функции
+{
+    //
+}
+function Mathematics()
+{
+    $(".MainMenu").hide();
+}
+function Physics()
+{
+    $(".MainMenu").hide();
+}
+function Chemisty()
+{
+    $(".MainMenu").hide();
+}
+function Geometry()
+{
+    $(".MainMenu").hide();
+}
+function Geography()
+{
+    $(".MainMenu").hide();
+}
